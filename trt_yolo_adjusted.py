176bf6d501256898f5de730930eb3a3ae06d1215
Zhenglin <1248184844@qq.com>
2021-03-23 17:09:12 +0800

    detection without deepsort

---
+++ trt_yolo_adjusted.py
@@ -0,0 +1,151 @@
+"""trt_yolo.py
+
+This script demonstrates how to do real-time object detection with
+TensorRT optimized YOLO engine.
+"""
+
+
+import os
+import time
+import argparse
+
+import cv2
+import numpy as np
+import pycuda.autoinit  # This is needed for initializing CUDA driver
+
+from utils.yolo_classes import get_cls_dict
+from utils.camera import add_camera_args, Camera
+from utils.display import open_window, set_display, show_fps
+from utils.visualization import BBoxVisualization
+from utils.yolo_with_plugins import get_input_shape, TrtYOLO
+
+
+WINDOW_NAME = 'TrtYOLODemo'
+
+kalman_list = []
+
+# 解析终端输入字符串
+def parse_args():
+    """Parse input arguments."""
+    desc = ('Capture and display live camera video, while doing '
+            'real-time object detection with TensorRT optimized '
+            'YOLO model on Jetson')
+    '''description = python3 trt_yolo.py --usb 0 -m yolov4-tiny-288'''
+    parser = argparse.ArgumentParser(description=desc)
+    parser = add_camera_args(parser)
+    parser.add_argument(
+        '-c', '--category_num', type=int, default=80,
+        help='number of object categories [80]')
+    parser.add_argument(
+        '-m', '--model', type=str, required=True,
+        help=('[yolov3|yolov3-tiny|yolov3-spp|yolov4|yolov4-tiny]-'
+              '[{dimension}], where dimension could be a single '
+              'number (e.g. 288, 416, 608) or WxH (e.g. 416x256)'))
+    parser.add_argument(
+        '-l', '--letter_box', action='store_true',
+        help='inference with letterboxed image [False]')
+    args = parser.parse_args()
+    return args
+
+
+# 筛选出行人类别
+def obj_filter(boxes, confs, clss):
+    '''
+    boxes: 
+    [[ 552   73  708  429]      
+    [ 535  154  586  309]
+    [   5   20  269  363]           # 注: (left, top), (left+w, top+h) 与输入帧尺度一致
+    [ 954  226 1166  343]
+    [1219  232 1279  440]
+    [ 837  235  975  285]
+    [ 901  236  999  304]]
+    confs: [0.5630284  0.4677704  0.6801914  0.6645249  0.58727616 0.44328296 0.3335283]
+    clss: [0. 0. 2. 2. 2. 2. 2.]    #注:0是人
+    '''
+    idxs = [i for i, c in enumerate(clss) if c == 0]
+    # 三元表达式写法不知道为什么报错，以后再精简代码
+    temp1 = []
+    temp2 = []
+    temp3 = []
+    for idx in idxs:
+        temp1.append(clss[idx])
+        temp2.append(boxes[idx])
+        temp3.append(confs[idx])
+    clss = temp1
+    boxes = temp2
+    confs = temp3
+
+    return boxes, confs, clss
+
+
+# 循环检测主函数
+def loop_and_detect(cam, trt_yolo, conf_th, vis):
+    """Continuously capture images from camera and do object detection.
+    # Arguments
+      cam: the camera instance (video source).
+      trt_yolo: the TRT YOLO object detector instance.
+      conf_th: confidence/score threshold for object detection.
+      vis: for visualization.
+    """
+    full_scrn = False
+    fps = 0.0
+    tic = time.time()
+    while True:
+        if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
+            break
+        img = cam.read()
+        global frame_h, frame_w
+        frame_h, frame_w = img.shape[:-1]
+
+        if img is None:
+            break
+        boxes, confs, clss = trt_yolo.detect(img, conf_th)  # 最重要的一行
+        # boxes, confs, clss = obj_filter(boxes, confs, clss)
+        
+        img = vis.draw_bboxes(img, boxes, confs, clss)
+        img = show_fps(img, fps)
+        cv2.imshow(WINDOW_NAME, img)
+        toc = time.time()
+        curr_fps = 1.0 / (toc - tic)
+        # calculate an exponentially decaying average of fps number
+        fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
+        tic = toc
+        key = cv2.waitKey(1)
+        if key == 27 or key == ord(' '):  # ESC key: quit program
+            break
+        elif key == ord('F') or key == ord('f'):  # Toggle fullscreen
+            full_scrn = not full_scrn
+            set_display(WINDOW_NAME, full_scrn)
+
+
+def main():
+    args = parse_args()
+    if args.category_num <= 0:
+        raise SystemExit('ERROR: bad category_num (%d)!' % args.category_num)
+    if not os.path.isfile('yolo/%s.trt' % args.model):
+        raise SystemExit('ERROR: file (yolo/%s.trt) not found!' % args.model)
+
+    cam = Camera(args)
+    if not cam.isOpened():
+        raise SystemExit('ERROR: failed to open camera!')
+    
+    # Definition of the parameters
+    max_cosine_distance = 0.4
+    nn_budget = None
+    nms_max_overlap = 1.0
+    cls_dict = get_cls_dict(args.category_num)
+    vis = BBoxVisualization(cls_dict)
+    h, w = get_input_shape(args.model)
+    trt_yolo = TrtYOLO(args.model, (h, w), args.category_num, args.letter_box)
+
+    open_window(
+        WINDOW_NAME, 'Camera TensorRT YOLO Demo',
+        cam.img_width, cam.img_height)
+    loop_and_detect(cam, trt_yolo, conf_th=0.3, vis=vis)
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
